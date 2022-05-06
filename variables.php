1403e95908938e8bd0fbe36ad8abe3ffc24a93b6
Cindie Jouvin <cindie.jouvin@gmail.com>
2022-05-06 15:54:47 +0200

    test appel de variable

---
+++ variables.php
@@ -0,0 +1,27 @@
+<?php
+
+$actors = [
+    [
+        'nom' => 'Formation&Co',
+        'description' => 'Formation&co est une association française présente sur tout le territoire.',
+        'logo' => 'https://zupimages.net/up/22/18/tegp.png',
+    ],
+    [
+        'nom' => 'Actor2',
+        'description' => '',
+        'logo' => 'https://zupimages.net/up/22/18/s86m.png',
+    ],
+    [
+        'nom' => 'Actor3',
+        'description' => '',
+        'logo' => 'https://zupimages.net/up/22/18/pzjh.png',
+    ],
+    [
+        'nom' => 'Actor4',
+        'description' => '',
+        'logo' => 'https://zupimages.net/up/22/18/m8st.png',
+    ],
+];
+?>
+
+
