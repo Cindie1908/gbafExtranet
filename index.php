9ac284ed79f1fb25b69e9181fcb622456295f53d
Cindie Jouvin <cindie.jouvin@gmail.com>
2022-05-09 11:54:41 +0200

    mise en place de la page de connexion

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GBAF Extranet</title>
+        <link rel="stylesheet" href="styles.css" type="text/css" >
+    </head>
+
 <?php
 try
 {
@@ -43,36 +51,33 @@ if (isset($_POST['username']) &&  isset($_POST['password'])) {
    Si utilisateur/trice est non identifié(e), on affiche le formulaire
 -->
 <?php if(!isset($loggedUser)): ?>
-<form action="index.php" method="post">
+<form class="formulaire-de-connexion" action="index.php" method="post">
     <!-- si message d'erreur on l'affiche -->
     <?php if(isset($errorMessage)) : ?>
         <div class="alert alert-danger" role="alert">
             <?php echo $errorMessage; ?>
         </div>
     <?php endif; ?>
-    <div class="mb-3">
-        <label for="username" class="form-label">Nom utilisateur</label>
-        <input type="htmlspecialchars" class="form-control" id="username" name="username" aria-describedby="username-help" placeholder="prénom.nom">
-        <div id="username-help" class="form-text">Le nom d'utilisateur à la création de compte.</div>
+    <img class="logo-header" alt="logo GRBF" src="https://zupimages.net/up/22/18/baeh.png")>
+    <p>Vous êtes sur l'extranet GBAF, pour accéder à la suite, merci de vous connecter :</p>
+    <div class="page-connex">
+        <div class="mb">
+            <label for="username" class="form-label">Nom utilisateur :</label>
+            <input type="htmlspecialchars" class="form-control" id="username" name="username" aria-describedby="username-help" placeholder="prénom.nom">
+        </div>
+        <div class="mb">
+            <label for="password" class="form-label">Mot de passe :</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
     </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Mot de passe</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
-    <button type="submit" class="btn btn-primary">Envoyer</button>
+    <button class="seconnecter" type="submit" class="btn btn-primary">Se connecter</button>
 </form>
 <!-- 
     Si utilisateur/trice bien connectée on affiche un message de succès
 -->
 <?php else: ?>
 
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>GBAF Extranet</title>
-        <link rel="stylesheet" href="styles.css" type="text/css" >
-    </head>
+    
     <body class="corpsite">
         <div class="container">
 
