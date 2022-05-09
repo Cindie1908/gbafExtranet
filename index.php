9810103177115a948ec289b8a3b7c0adf94c5cd6
Cindie Jouvin <cindie.jouvin@gmail.com>
2022-05-09 11:23:34 +0200

    raccordement à la table users et connexion user

--- index.php
+++ index.php
@@ -1,23 +1,103 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>GBAF Extranet</title>
-    <link rel="stylesheet" href="styles.css" type="text/css" >
-</head>
-<body class="corpsite">
-    <div class="container">
+<?php
+try
+{
+    //connection à MySQL
+	$mysqlUser = new PDO('mysql:host=localhost;dbname=gbaf_extranet;charset=utf8', 'root', 'root');
+}
+catch (Exception $e)
+{
+    //récupération de l'erreur
+    die('Erreur : ' . $e->getMessage());
+}
 
-    <!-- Navigation -->
-    <?php include_once('header.php'); ?>
+//récupération des données de la table actors
+$sqlQuery = "SELECT * FROM `users`";
+$usersStatement = $mysqlUser->prepare($sqlQuery);
+$usersStatement->execute();
+$users = $usersStatement->fetchAll();   
+?>
 
-    <!-- Entête -->
-    <?php include_once('title.php'); ?>
+<?php
 
-    <!-- Liste -->
-    <?php include_once('vignette.php'); ?>   
+// Validation du formulaire
+if (isset($_POST['username']) &&  isset($_POST['password'])) {
+    foreach ($users as $user) {
+        if (
+            $user['username'] === $_POST['username'] &&
+            $user['password'] === $_POST['password']
+        ) {
+            $loggedUser = [
+                'username' => $user['username'],
+            ];
+        } else {
+            $errorMessage = sprintf('Erreur identifiée : (%s/%s)',
+                $_POST['username'],
+                $_POST['password']
+            );
+        }
+    }
+}
+?>
 
-    <!-- pied -->
-    <?php include_once('footer.php'); ?>
-</body>
-</html>
+<!--
+   Si utilisateur/trice est non identifié(e), on affiche le formulaire
+-->
+<?php if(!isset($loggedUser)): ?>
+<form action="index.php" method="post">
+    <!-- si message d'erreur on l'affiche -->
+    <?php if(isset($errorMessage)) : ?>
+        <div class="alert alert-danger" role="alert">
+            <?php echo $errorMessage; ?>
+        </div>
+    <?php endif; ?>
+    <div class="mb-3">
+        <label for="username" class="form-label">Nom utilisateur</label>
+        <input type="htmlspecialchars" class="form-control" id="username" name="username" aria-describedby="username-help" placeholder="prénom.nom">
+        <div id="username-help" class="form-text">Le nom d'utilisateur à la création de compte.</div>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Mot de passe</label>
+        <input type="password" class="form-control" id="password" name="password">
+    </div>
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+</form>
+<!-- 
+    Si utilisateur/trice bien connectée on affiche un message de succès
+-->
+<?php else: ?>
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GBAF Extranet</title>
+        <link rel="stylesheet" href="styles.css" type="text/css" >
+    </head>
+    <body class="corpsite">
+        <div class="container">
+
+        <!-- Si l'utilisateur existe, on affiche la page -->
+        <?php if(isset($loggedUser)): ?>
+
+            <!-- Navigation -->
+            <?php include_once('header.php'); ?>
+
+        
+            <!-- Entête -->
+            <?php include_once('title.php'); ?>
+
+            <!-- Liste -->
+            <?php include_once('vignette.php'); ?>  
+
+            <!-- pied -->
+            <?php include_once('footer.php'); ?>
+
+        <?php endif; ?>
+
+    </body>
+    </html>
+
+<?php endif; ?>
+
+
+
