f563ea9d91daa5f6c637e220c38d203b26478492
Cindie Jouvin <cindie.jouvin@gmail.com>
2022-05-09 10:48:33 +0200

    raccordement à la BDD table actor

--- vignette.php
+++ vignette.php
@@ -1,13 +1,37 @@
-<?php include_once('variables.php'); ?>
-    <?foreach ($actors as $actor): ?> 
+<?php
+try
+{
+    //connection à MySQL
+	$mysqlClient = new PDO('mysql:host=localhost;dbname=gbaf_extranet;charset=utf8', 'root', 'root');
+}
+catch (Exception $e)
+{
+    //récupération de l'erreur
+    die('Erreur : ' . $e->getMessage());
+}
+
+//récupération des données de la table actors
+$sqlQuery = "SELECT * FROM `actors`";
+$Statement = $mysqlClient->prepare($sqlQuery);
+$Statement->execute();
+$actors = $Statement->fetchAll();   
+
+foreach ($actors as $actor) {
+    ?>
         <div class="vignette">
             <div >
-                <img class="logo-vignette" alt=<?php echo $actor['logo']; ?> src="<?php echo $actor['logo']; ?>")>
+                <img class="logo-vignette" alt=<?php echo $actor['Logo']; ?> src="<?php echo $actor['Logo']; ?>")>
             </div>
             <div class="displayvignette">
-                <p><?php echo $actor['nom']; ?></p>
-                <p><?php echo substr($actor['description'],0,120); ?> ...</p>
+            <p><?php echo $actor['Titre']; ?></p>
+                <p><?php echo substr($actor['Description'],0,120); ?> ...</p>
                 <button class="lirelasuite">Lire la suite</button>
             </div>
     </div>
-    <?php endforeach ?>
+    <?php
+    }
+    ?>
+
+
+
+
